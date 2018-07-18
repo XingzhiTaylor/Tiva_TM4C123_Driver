264708598c04fb94542a5446b7a7087afb0e4c94
XingzhiTaylor <xingzhitaylor@gmail.com>
2018-07-17 23:00:55 -0400

    Added clock enable functions and configuration data structure

--- SPI Driver/spi_driver.h
+++ SPI Driver/spi_driver.h
@@ -37,20 +37,41 @@
 #define SPI_LOOPBACK																						1
 
 /*********************Bit Definitions for ISR Register*********************/
-#define SPI_BUSY 														 ( (uint32_t) 1 << 4)
-#define SPI_FIFO_FULL												 ( (uint32_t) 1 << 3)
-#define SPI_FIFO_NOT_EMPTY									 ( (uint32_t) 1 << 2)
-#define SPI_FIFO_NOT_FULL										 ( (uint32_t) 1 << 1)	
-#define SPI_FIFO_EMPTY											 ( (uint32_t) 1 << 0)	
+#define SPI_BUSY 														( (uint32_t) 1 << 4 )
+#define SPI_FIFO_FULL												( (uint32_t) 1 << 3 )
+#define SPI_FIFO_NOT_EMPTY									( (uint32_t) 1 << 2 )
+#define SPI_FIFO_NOT_FULL										( (uint32_t) 1 << 1 )	
+#define SPI_FIFO_EMPTY											( (uint32_t) 1 << 0 )	
 
 /*********************Bit Definitions for ICC Register*********************/
 #define SPI_SYS_CLK													 									0x0
 #define SPI_PIOSC														 									0x5
 
 /***************************Clock Init Macros*****************************/
-// To be implemented
+#define RCC_SSI0_CLK_ENABLE()       volatile unsigned long delay;\
+                                    SYSCTL->RCGCSSI |= (0x01 << 0);\
+                                    delay = SYSCTL->RCGCSSI;
+#define RCC_SSI1_CLK_ENABLE()       volatile unsigned long delay;\
+                                    SYSCTL->RCGCSSI |= (0x01 << 1);\
+                                    delay = SYSCTL->RCGCSSI;
+#define RCC_SSI2_CLK_ENABLE()       volatile unsigned long delay;\
+                                    SYSCTL->RCGCSSI |= (0x01 << 2);\
+                                    delay = SYSCTL->RCGCSSI;
+#define RCC_SSI3_CLK_ENABLE()       volatile unsigned long delay;\
+                                    SYSCTL->RCGCSSI |= (0x01 << 3);\
+                                    delay = SYSCTL->RCGCSSI;
 
 /*************************SPI Data Structures*****************************/
-// To be implemented
+typedef struct {
+	uint32_t Clock_phase;
+	uint32_t Clock_polarity;
+	uint32_t Frame_format;
+	uint32_t Mode;							// Slave or master
+	uint32_t EOT_enable;
+	uint32_t Loopback;
+	uint32_t Clock;
+	uint32_t Clock_prescale;		// CPSR Register
+	uint32_t Clock_scaler;      // SCR value in CR0 register
+} spi_conf_t;
 
 #endif
